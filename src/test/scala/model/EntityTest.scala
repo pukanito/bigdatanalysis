93d7d44090c5ff630b244d4c3329b6e01dc3f441
jaap <jaap@xxx.nl>
2012-12-03 20:13:25 +0100

    More tests

--- src/test/scala/model/EntityTest.scala
+++ src/test/scala/model/EntityTest.scala
@@ -5,6 +5,7 @@ import org.scalatest.FunSpec
 class EntityTest extends FunSpec {
 
     describe("Entity") {
+
         it("should be comparable to other entities") {
             val strAttr = StringAttribute("MY_STRING")
             val strValue1 = strAttr.createValue("some string value")
@@ -14,8 +15,22 @@ class EntityTest extends FunSpec {
             val entity2 = entity1.update(strValue2)
             assert(entity0.update(strValue1) === entity1)
             assert(entity1 != entity2)
-            println(entity1)
-            println(entity2)
+        }
+        
+        it("should be able to contain different attribute types") {
+            val attr1 = StringAttribute("ATTR1")
+            val attr2 = StringAttribute("ATTR2")
+            val attr3 = IntAttribute("ATTR3")
+            val val1 = attr1.createValue("aaa")
+            val val2 = attr2.createValue("bbb")
+            val val3 = attr3.createValue(123)
+            val entity = DefaultEntity("ENTITY_1", Map(
+                    val1.attribute.id -> val1,
+                    val2.attribute.id -> val2,
+                    val3.attribute.id -> val3))
+            assert(entity.values("ATTR1") === val1)
+            assert(entity.values("ATTR2") === val2)
+            assert(entity.values("ATTR3") === val3)
         }
     }
 }
