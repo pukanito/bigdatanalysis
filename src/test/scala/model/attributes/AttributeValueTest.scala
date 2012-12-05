d9d2f56eff4e9b0365e27feb1618151f42c58513
Pukanito Forager <pukanito@gmail.com>
2012-12-05 17:12:51 +0100

    refactoring

--- src/test/scala/model/attributes/AttributeValueTest.scala
+++ src/test/scala/model/attributes/AttributeValueTest.scala
@@ -5,6 +5,14 @@ import org.scalatest.matchers.ShouldMatchers
 
 class AttributeValueTest extends FunSpec with ShouldMatchers {
 
+  private class AttributeTestValue(
+    attributeDefinition: AttributeDefinition,
+    parentAttributeValue: Option[AttributeValue] = None
+  ) extends AttributeValue(attributeDefinition, parentAttributeValue) {
+    
+    def value: Any = { 0 }
+  }
+  
   describe("AttributeValue") {
     
     it("should be associated with an AttributeDefinition") {
@@ -18,12 +26,4 @@ class AttributeValueTest extends FunSpec with ShouldMatchers {
     it("can be associated with a parent AttributeValue") (pending)
     
   }
-}
-
-class AttributeTestValue(
-  attributeDefinition: AttributeDefinition,
-  parentAttributeValue: Option[AttributeValue] = None
-) extends AttributeValue(attributeDefinition, parentAttributeValue) {
-    
-  def value: Any = { 0 }
 }
