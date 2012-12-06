044f20a052c555baa0d4ea008024dd18fe41a011
Pukanito Forager <pukanito@gmail.com>
2012-12-06 16:00:35 +0100

    added requirements

--- src/test/scala/model/attributes/AttributeValueTest.scala
+++ src/test/scala/model/attributes/AttributeValueTest.scala
@@ -9,21 +9,23 @@ class AttributeValueTest extends FunSpec with ShouldMatchers {
     attributeDefinition: AttributeDefinition,
     parentAttributeValue: Option[AttributeValue] = None
   ) extends AttributeValue(attributeDefinition, parentAttributeValue) {
-    
+
     def value: Any = { 0 }
   }
-  
+
   describe("AttributeValue") {
-    
+
     it("should be associated with an AttributeDefinition") {
       val d = new AttributeDefinition("testid")
       val v: AttributeValue = new AttributeTestValue(d)
       v.attributeDefinition should equal (d)
     }
-    
+
     it("can be a root attribute, i.e. has no parent AttributeValue") (pending)
-    
-    it("can be associated with a parent AttributeValue") (pending)
-    
+
+    it("can be associated with a single parent AttributeValue") (pending)
+
+    it("can be associated with multiple parent AttributeValues") (pending)
+
   }
 }
