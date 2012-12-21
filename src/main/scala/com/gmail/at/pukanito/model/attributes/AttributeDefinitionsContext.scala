c1338eee69544f392c1a997ed4cb5c9db8300cf2
Pukanito Forager <pukanito@gmail.com>
2012-12-21 15:11:33 +0100

    Added behavior and empty class for attribute definitions context

---
+++ src/main/scala/com/gmail/at/pukanito/model/attributes/AttributeDefinitionsContext.scala
@@ -0,0 +1,10 @@
+package com.gmail.at.pukanito.model.attributes
+
+/**
+ * A context for maintaining and using AttributeDefinitions.
+ *
+ * @constructor Create an empty attribute definitions context.
+ */
+class AttributeDefinitionsContext {
+
+}
