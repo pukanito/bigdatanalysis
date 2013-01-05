97d368cd950f11b40ee8b8cfe547fd03bbbc3b0e
Pukanito Forager <pukanito@gmail.com>
2013-01-05 17:53:15 +0100

    Modified requirements

--- src/test/scala/com/gmail/at/pukanito/model/attributes/AttributeModelTest.scala
+++ src/test/scala/com/gmail/at/pukanito/model/attributes/AttributeModelTest.scala
@@ -7,21 +7,19 @@ class AttributeModelTest extends FunSpec with ShouldMatchers {
 
   describe("AttributeModel") {
 
-    it("should be possible to add attribute definitions to the model") (pending)
+    it("should model parent-child relations between attribute definitions") (pending)
 
-    it("should be possible to model parent-child relations between attribute definitions") (pending)
+    it("should model attribute definitions without a parent as root attributes") (pending)
 
-    it("attribute definitions in the model without a parent are root attributes") (pending)
+    it("should be possible to uniquely identify a modelled attribute definition by its path (root -> child -> child -> ...)") (pending)
 
-    it("should be possible to retrieve a modelled attribute definition by its path (root -> child -> child -> ...") (pending)
+    it("should throw a NoSuchElementException when a non modelled path is retrieved") (pending)
 
-    it("should throw a NoSuchElementException when a non modelled attribute definition is retrieved by its path") (pending)
+    it("should throw a DuplicateAttributeException when an attribute definition is added with an already existing path") (pending)
 
-    it("should throw a DuplicateAttributeException when an attribute definition is added with an already modelled identifier") (pending)
+    it("should throw a NoSuchElementException when a deleted attribute definition is retrieved by its path") (pending)
 
-    it("should throw a NoSuchElementException when a deleted attribute definition is retrieved by its identifier") (pending)
-
-    it("should throw a NoSuchElementException when a renamed attribute definition is retrieved by its old identifier") (pending)
+    it("should throw a NoSuchElementException when a renamed attribute definition is retrieved by its old path") (pending)
 
     it("should be possible to retrieve the modelled children of a modelled attribute definition") (pending)
 
