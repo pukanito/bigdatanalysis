3b64815b40a7358e80f26d34f30bb805f780d942
Pukanito Forager <pukanito@gmail.com>
2013-01-13 15:51:25 +0100

    Implement some graph item tests

--- src/main/scala/com/gmail/at/pukanito/model/container/GraphItem.scala
+++ src/main/scala/com/gmail/at/pukanito/model/container/GraphItem.scala
@@ -4,17 +4,34 @@ package com.gmail.at.pukanito.model.container
  * Trait for making items graph compatible.
  */
 trait GraphItem {
+  type GraphItemKey = Map[String, Any]
   private var parentValues: Set[GraphItem] = Set()
-  private var childrenMap: Map[String, GraphItem] = Map()
+  private var childrenMap: Map[GraphItemKey, GraphItem] = Map()
 
   /**
-   * Set of parent graph items of this graph item.
+   * The key of a graph item. Should be immutable!
+   */
+  def key: GraphItemKey
+
+  /**
+   * Set of parent graph items of this graph item. Empty when
+   * this is a root item.
    */
   def parents: Set[GraphItem] = { parentValues }
 
   /**
    * Map of child graph items of this graph items.
    */
-  def children: Map[String, GraphItem] = { childrenMap }
+  def children: Map[GraphItemKey, GraphItem] = { childrenMap }
+
+  /**
+   * Add a new child to this graph item.
+   *
+   * @param child The child to add.
+   */
+  def +=(value: GraphItem) = {
+    childrenMap += (value.key -> value)
+    value.parentValues += this
+  }
 
 }
--- src/test/scala/com/gmail/at/pukanito/model/container/GraphItemTest.scala
+++ src/test/scala/com/gmail/at/pukanito/model/container/GraphItemTest.scala
@@ -3,29 +3,44 @@ package com.gmail.at.pukanito.model.container
 import org.scalatest.FunSpec
 import org.scalatest.matchers.ShouldMatchers
 
+class TestGraphItem(val k: Int) extends GraphItem {
+  override def key: GraphItemKey = { Map("A" -> k) }
+}
+
 class GraphItemTest extends FunSpec with ShouldMatchers {
 
   describe("GraphItem") {
 
-    it("should model parent-child relations between data graph items") (pending)
+    it("should compare keys of different graph items with the same key as equal") {
+      val t1 = new TestGraphItem(1)
+      val t2 = new TestGraphItem(1)
+      t1.key should equal (t2.key)
+    }
+
+    it("should compare keys of different graph items with different keys as not equal") {
+      val t1 = new TestGraphItem(1)
+      val t2 = new TestGraphItem(2)
+      t1.key should not equal (t2.key)
+    }
+
+    it("should model parent-child relations between graph items") (pending)
 
     it("should throw an exception when a cycle is detected") (pending)
 
-    it("should be possible to uniquely identify a data graph item by its path (root / childkey / childkey / ...)") (pending)
+    it("should be possible to uniquely identify a graph item by its path (root / childkey / childkey / ...)") (pending)
 
     it("should throw a NoSuchElementException when a non existing path is retrieved") (pending)
 
-    it("should throw a DuplicateAttributeException when a data graph item is added with an already existing path") (pending)
+    it("should throw a DuplicateAttributeException when a graph item is added with an already existing path") (pending)
 
-    it("should throw a NoSuchElementException when a deleted data graph item is retrieved by its path") (pending)
+    it("should throw a NoSuchElementException when a deleted graph item is retrieved by its path") (pending)
 
-    it("should throw a NoSuchElementException when a renamed data graph item is retrieved by its old path") (pending)
+    it("should throw a NoSuchElementException when a renamed graph item is retrieved by its old path") (pending)
 
-    it("should be possible to retrieve the children of a data graph item") (pending)
+    it("should be possible to retrieve the children of a graph item") (pending)
 
-    it("should be possible to retrieve the parents of a data graph item") (pending)
+    it("should be possible to retrieve the parents of a graph item") (pending)
 
   }
-
 
 }
