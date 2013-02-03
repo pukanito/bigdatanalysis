fb7da0dbd8a11a592aaaf2f22fd74d56dd8aab69
Pukanito Forager <pukanito@gmail.com>
2013-02-03 14:37:07 +0100

    Modify attribute definition to allow optional initial children and parents on creation

--- src/main/scala/com/gmail/at/pukanito/model/attributes/AttributeDefinition.scala
+++ src/main/scala/com/gmail/at/pukanito/model/attributes/AttributeDefinition.scala
@@ -1,16 +1,21 @@
 package com.gmail.at.pukanito.model.attributes
 
-import com.gmail.at.pukanito.model.container.GraphItem
+import com.gmail.at.pukanito.model.container.{GraphItem,GraphItemKey}
 
 /**
  * The base class for all attribute definitions.
  *
  * @constructor Create a base attribute definition with a specific id.
  * @param attributeId The attribute identifier to assign to the AttributeDefinition
+ * @param attributeValueKeyIds List of identifiers that are part of the key of an attribute value
+ *          that is associated with this attribute definition.
  */
 class AttributeDefinition (
-  val attributeId: AttributeIdentifier
-) extends GraphItem[AttributeDefinition] {
+  val attributeId: AttributeIdentifier,
+  val attributeValueKeyIds: List[AttributeIdentifier] = List(),
+  initialChildren: Set[GraphItem[AttributeDefinition]] = Set[GraphItem[AttributeDefinition]](),
+  initialParents: Set[GraphItem[AttributeDefinition]] = Set[GraphItem[AttributeDefinition]]()
+) extends GraphItem[AttributeDefinition](initialChildren, initialParents) {
 
   override def key = attributeId.toString
 
