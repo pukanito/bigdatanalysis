00eda9334198646e3fa0f82d8b707ecf43dfd26e
Pukanito Forager <pukanito@gmail.com>
2013-02-06 13:53:08 +0100

    Refactoring

--- src/main/scala/com/gmail/at/pukanito/model/dsl/AttributeModel.scala
+++ src/main/scala/com/gmail/at/pukanito/model/dsl/AttributeModel.scala
@@ -11,8 +11,6 @@ trait AttributeModel {
 
   private val IntegerAttribute = new isOfType("Integer")
 
-  private class hasName {  }
-
   private class hasKey(val h: hasWord) {
     def and(id: AttributeIdentifier) = { h.attributeValueKeyIds += id; this }
   }
@@ -26,15 +24,13 @@ trait AttributeModel {
   }
 
   private class hasWord {
-    private var attributeId: AttributeIdentifier = ""
     private[AttributeModel] var attributeValueKeyIds: Set[AttributeIdentifier] = Set()
     private[AttributeModel] var initialChildren: Set[AttributeIdentifier] = Set()
     private[AttributeModel] var initialParents: Set[AttributeIdentifier] = Set()
-    private[AttributeModel] def clear: Unit = { attributeId = ""; attributeValueKeyIds = Set(); initialChildren = Set(); initialParents = Set() }
-    private[AttributeModel] def build: AttributeDefinition = {
-      new AttributeDefinition(attributeId, attributeValueKeyIds.toList, initialChildren.map(definedAttributes(_)), initialParents.map(definedAttributes(_)))
+    private[AttributeModel] def clear: Unit = { attributeValueKeyIds = Set(); initialChildren = Set(); initialParents = Set() }
+    private[AttributeModel] def build(id: String): AttributeDefinition = {
+      new AttributeDefinition(id, attributeValueKeyIds.toList, (initialChildren ++ attributeValueKeyIds).map(definedAttributes(_)), initialParents.map(definedAttributes(_)))
     }
-    def name(id: AttributeIdentifier): hasName = { attributeId = id; new hasName }
     def keys(ids: AttributeIdentifier*): hasKey = { attributeValueKeyIds ++= ids; new hasKey(this)  }
     def parents(ids: AttributeIdentifier*): hasParents = { initialParents ++= ids; new hasParents(this) }
     def children(ids: AttributeIdentifier*): hasChildren = { initialChildren ++= ids; new hasChildren(this) }
@@ -43,10 +39,10 @@ trait AttributeModel {
   protected[AttributeModel] val has = new hasWord
 
   private class attributeWord(val t: isOfType = IntegerAttribute) {
-    def apply(body: => Unit): AttributeDefinition = {
+    def apply(id: String)(body: => Unit): AttributeDefinition = {
       has.clear
       body
-      val a = has.build
+      val a = has.build(id)
       definedAttributes += a.attributeId -> a
       return a
     }
@@ -60,8 +56,7 @@ object testmodel extends AttributeModel {
 
   val x = "testprefix"
 
-  attribute {
-    has name x + "test"
+  attribute(x + "test") {
     has keys "A" and "B"
     has parents "P" and "Q"
     has children "X" and "Y" and "Z"
