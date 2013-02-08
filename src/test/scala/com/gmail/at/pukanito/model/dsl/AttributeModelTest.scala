35a438169b4bfee011d8941ed84e73e753566ac6
Pukanito Forager <pukanito@gmail.com>
2013-02-08 11:13:39 +0100

    Layout

--- src/test/scala/com/gmail/at/pukanito/model/dsl/AttributeModelTest.scala
+++ src/test/scala/com/gmail/at/pukanito/model/dsl/AttributeModelTest.scala
@@ -7,18 +7,6 @@ class AttributeModelTest extends FunSpec with ShouldMatchers {
 
   describe("AttributeModel") {
 
-    it("should not throw an exception when an attribute with the same identifier is defined in different models") {
-      object testmodel1 extends AttributeModel {
-        attribute("test") { }
-      }
-      object testmodel2 extends AttributeModel {
-        attribute("test") { }
-      }
-      // Due to lazy initialization of an object it has to be pushed a little bit.
-      testmodel1.attributes
-      testmodel2.attributes
-    }
-
     it("should be possible to create a parent-child hierarchy of attribute definitions") (pending)
 
     it("should throw an exception when a non existing child is added on creation") {
@@ -69,6 +57,18 @@ class AttributeModelTest extends FunSpec with ShouldMatchers {
         intercept[RuntimeException] { attribute("test") { has keys "key2" } }
       }
       testmodel.attributes
+    }
+
+    it("should not throw an exception when an attribute with the same identifier is defined in different models") {
+      object testmodel1 extends AttributeModel {
+        attribute("test") { }
+      }
+      object testmodel2 extends AttributeModel {
+        attribute("test") { }
+      }
+      // Due to lazy initialization of an object it has to be pushed a little bit.
+      testmodel1.attributes
+      testmodel2.attributes
     }
 
     it("should be possible to import a copy of another model") (pending)
