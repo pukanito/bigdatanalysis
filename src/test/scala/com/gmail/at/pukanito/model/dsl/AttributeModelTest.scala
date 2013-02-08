cc0f8dfe370f76ed3888fbb7cea7f02e97688dc1
Pukanito Forager <pukanito@gmail.com>
2013-02-08 12:07:37 +0100

    Implemented hierarchy test

--- src/test/scala/com/gmail/at/pukanito/model/dsl/AttributeModelTest.scala
+++ src/test/scala/com/gmail/at/pukanito/model/dsl/AttributeModelTest.scala
@@ -7,7 +7,35 @@ class AttributeModelTest extends FunSpec with ShouldMatchers {
 
   describe("AttributeModel") {
 
-    it("should be possible to create a parent-child hierarchy of attribute definitions") (pending)
+    it("should be possible to create a parent-child hierarchy of attribute definitions") {
+      object testmodel extends AttributeModel {
+        attribute("key1") { }
+        attribute("key2") { }
+        attribute("parent") { has keys("key1", "key2") }
+        attribute("child1") { has parents "parent" }
+        attribute("child2") {  has parents "parent" }
+        attribute("child3") {  has parents "parent" }
+        attribute("child1ofchild1") { has parents "child1" and "parent" }
+        attribute("child2ofchild1") { has parents "child1" }
+        attribute("child1ofchild3") { has parents "child3" }
+      }
+      testmodel.attributes("parent").parents should have size (0)
+      // Next one has keys and children as children
+      testmodel.attributes("parent").children should have size (6)
+      testmodel.attributes("parent").attributeValueKeyIds should have size (2)
+      testmodel.attributes("child1").parents should have size (1)
+      testmodel.attributes("child1").children should have size (2)
+      testmodel.attributes("child2").parents should have size (1)
+      testmodel.attributes("child2").children should have size (0)
+      testmodel.attributes("child3").parents should have size (1)
+      testmodel.attributes("child3").children should have size (1)
+      testmodel.attributes("child1ofchild1").parents should have size (2)
+      testmodel.attributes("child1ofchild1").children should have size (0)
+      testmodel.attributes("child2ofchild1").parents should have size (1)
+      testmodel.attributes("child2ofchild1").children should have size (0)
+      testmodel.attributes("child1ofchild3").parents should have size (1)
+      testmodel.attributes("child1ofchild3").children should have size (0)
+    }
 
     it("should throw an exception when a non existing child is added on creation") {
       object testmodel extends AttributeModel {
