8e1ea8e22566f92cc2342df634f8c28ae69791c3
Pukanito Forager <pukanito@gmail.com>
2013-02-23 21:19:36 +0100

    Comments

--- src/main/scala/com/gmail/at/pukanito/model/attributes/AttributeDefinition.scala
+++ src/main/scala/com/gmail/at/pukanito/model/attributes/AttributeDefinition.scala
@@ -5,8 +5,12 @@ import com.gmail.at.pukanito.model.container.{GraphItem,GraphItemKey}
 /**
  * The base class for all attribute definitions.
  *
- * @constructor Create a base attribute definition with a specific id.
- * @param attributeId The attribute identifier to assign to the AttributeDefinition
+ * An attribute definition defines a data structure that consists of data values
+ * that are related to each other in a parent child hierarchy.
+ *
+ * @constructor Create a base attribute definition with a specific attribute id.
+ * @param attributeId The attribute identifier to assign to the AttributeDefinition. Every
+ *          attribute definition has a unique attribute identifier.
  * @param attributeValueKeyIds List of identifiers that are part of the key of an attribute value
  *          that is associated with this attribute definition.
  * @param initialChildren List of attribute definitions that will be added as children of this
