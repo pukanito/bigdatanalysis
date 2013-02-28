abe17e05fb6723efd61a91cde913e018b66764d8
Pukanito Forager <pukanito@gmail.com>
2013-02-28 13:37:48 +0100

    Add requirements

--- src/test/scala/com/gmail/at/pukanito/model/store/GraphItemStoreTest.scala
+++ src/test/scala/com/gmail/at/pukanito/model/store/GraphItemStoreTest.scala
@@ -173,6 +173,23 @@ class GraphItemStoreTest extends FunSpec with ShouldMatchers {
       store(GraphPath(t1.key, t2a.key)).first should equal (r2a)
     }
 
+    it("should throw an exception or return None when a non existing path is retreived") {
+      val store = new MemoryMapGraphItemStore[TestSimpleGraphItem]
+      val t1 = new TestSimpleGraphItem("A", 10)
+      val t2a = new TestSimpleGraphItem("Ba", 20)
+      val t2b = new TestSimpleGraphItem("Bb", 20)
+      val t2c = new TestSimpleGraphItem("Bc", 30)
+      val t3 = new TestSimpleGraphItem("C", 30)
+      t1 += t2b
+      t1 += t2a
+      t2a += t3
+      store.put(t1)
+      intercept[NoSuchElementException] { store(t1.key, t2c.key) }
+      store.get(t2a.key).first should equal (None)
+//      store.get(GraphPath(t1.key, t2c.key)).first should equal (None)
+      (pending)
+    }
+
     it("should be possible to retrieve all graphs with a specific sub path") (pending)
 
     it("should be possible to create a graph by its path") {
