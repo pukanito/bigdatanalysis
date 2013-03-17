6297e25e2823dd2f27ebf36485142928b63db313
Pukanito Forager <pukanito@gmail.com>
2013-03-17 15:19:03 +0100

    Trying out a wrapper around Config

--- src/main/scala/com/gmail/at/pukanito/controller/config/Configuration.scala
+++ src/main/scala/com/gmail/at/pukanito/controller/config/Configuration.scala
@@ -2,9 +2,14 @@ package com.gmail.at.pukanito.controller.config
 
 import com.typesafe.config._
 
+/**
+ * Wrapper for https://github.com/typesafehub/config
+ */
 object Configuration {
 
   private val conf = ConfigFactory.load
 
-  def hasPath(path: String) = conf.hasPath(path)
+  def hasPath(path: String, spec: String = "") =
+    if (spec == "") conf.hasPath(path)
+    else conf.getConfig(spec).hasPath(path)
 }
--- src/test/scala/com/gmail/at/pukanito/controller/config/ConfigurationTest.scala
+++ src/test/scala/com/gmail/at/pukanito/controller/config/ConfigurationTest.scala
@@ -8,7 +8,8 @@ class ConfigurationTest extends FunSpec with ShouldMatchers {
   describe("Configuration") {
 
     it("should be possible to retrieve default values") {
-      Configuration.hasPath("test.foo.bar") should be (true)
+      Configuration.hasPath("foo.bar") should be (false)
+      Configuration.hasPath("foo.bar", "test") should be (true)
     }
 
     it("should be possible to retrieve specific values") (pending)
