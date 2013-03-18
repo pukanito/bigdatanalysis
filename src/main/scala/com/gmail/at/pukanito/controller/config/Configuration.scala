2b0c40a929e6eb0edad3c255b774e0928446910b
Pukanito Forager <pukanito@gmail.com>
2013-03-18 17:54:47 +0100

    Comments

--- src/main/scala/com/gmail/at/pukanito/controller/config/Configuration.scala
+++ src/main/scala/com/gmail/at/pukanito/controller/config/Configuration.scala
@@ -8,6 +8,27 @@ import com.typesafe.config._
  * Get configuration items for a specific environment and specification, or a default if a
  * specific platform and/or specification is not defined.
  *
+ * With environment and specification given the path priorization order will be:
+ *
+ *  1.  environment.specification.path
+ *  2.  specification.path
+ *  3.  environment.path
+ *  4.  path
+ *
+ * With only environment:
+ *
+ *  1.  environment.path
+ *  2.  path
+ *
+ * With only specification:
+ *
+ *  1. specification.path
+ *  2. path
+ *
+ * Without environment or specification:
+ *
+ *  1. path
+ *
  * @constructor Open a configuration for a specific environment.
  * @param environment The name of the environment.
  */
@@ -20,11 +41,8 @@ class Configuration (
    * Returns the highest prioritized path that can be created from 'environment',
    * 'spec' and 'path' or None.
    *
-   * Priority of path is:
-   * 1. environment.spec.path
-   * 2. spec.path
-   * 3. environment.path
-   * 4. path
+   * @param path Base path to the configuration value.
+   * @param spec Optional specialized value for the configuration value.
    */
   def whichPath(path: String, spec: String = ""): Option[String] = {
     ((if (!spec.isEmpty) List(spec+"."+path) else List()) ++  List(path)).map(
@@ -34,6 +52,9 @@ class Configuration (
 
   /**
    * Returns true when a path has a configuration value defined.
+   *
+   * @param path Base path to the configuration value.
+   * @param spec Optional specialized value for the configuration value.
    */
   def hasPath(path: String, spec: String = ""): Boolean = {
     ! whichPath(path, spec).isEmpty
@@ -41,6 +62,9 @@ class Configuration (
 
   /**
    * Returns the string value of a path.
+   *
+   * @param path Base path to the configuration value.
+   * @param spec Optional specialized value for the configuration value.
    */
   def getString(path: String, spec: String = ""): String = {
     whichPath(path, spec) match {
