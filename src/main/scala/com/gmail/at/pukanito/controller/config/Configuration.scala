e26058d886e419faf0d714e99e138f84fd0eae62
Pukanito Forager <pukanito@gmail.com>
2013-03-18 10:18:38 +0100

    Comments Improved test

--- src/main/scala/com/gmail/at/pukanito/controller/config/Configuration.scala
+++ src/main/scala/com/gmail/at/pukanito/controller/config/Configuration.scala
@@ -3,13 +3,29 @@ package com.gmail.at.pukanito.controller.config
 import com.typesafe.config._
 
 /**
- * Wrapper for https://github.com/typesafehub/config
+ * Wrapper for https://github.com/typesafehub/config.
+ *
+ * Get configuration items for a specific environment and specification, or a default if a
+ * specific platform and/or specification is not defined.
+ *
+ * @constructor Open a configuration for a specific environment.
+ * @param environment The name of the environment.
  */
 class Configuration (
   private val environment: String = ""
 ) {
   private val conf = ConfigFactory.load
 
+  /**
+   * Check all possible paths which can be created from 'environment', 'spec' and 'path'
+   * and return the highest prioritized one or None.
+   *
+   * Priority of path is:
+   * 1. environment.spec.path
+   * 2. spec.path
+   * 3. environment.path
+   * 4. path
+   */
   def whichPath(path: String, spec: String = ""): Option[String] = {
     ((if (!spec.isEmpty) List(spec+"."+path) else List()) ++  List(path)).map(
         (p) => (if (!environment.isEmpty) List(environment+"."+p) else List()) ++  List(p)
--- src/test/scala/com/gmail/at/pukanito/controller/config/ConfigurationTest.scala
+++ src/test/scala/com/gmail/at/pukanito/controller/config/ConfigurationTest.scala
@@ -9,24 +9,27 @@ class ConfigurationTest extends FunSpec with ShouldMatchers {
 
     it("should find the highest prioritized path") {
       new Configuration() whichPath("test") should equal (Some("test"))
-      new Configuration() whichPath("test.test") should equal (Some("test.test"))
+      new Configuration() whichPath("test.foo") should equal (Some("test.foo"))
       new Configuration() whichPath("test.bar") should equal (Some("test.bar"))
-      new Configuration() whichPath("test.test.bar") should equal (Some("test.test.bar"))
+      new Configuration() whichPath("test.foo.bar") should equal (Some("test.foo.bar"))
+      new Configuration() whichPath("foo.bar") should equal (Some("foo.bar"))
+      new Configuration() whichPath("spec.bar") should equal (Some("spec.bar"))
 
-      new Configuration("test") whichPath("test") should equal (Some("test.test"))
-      new Configuration("test") whichPath("test.test") should equal (Some("test.test"))
-      new Configuration("test") whichPath("test.bar") should equal (Some("test.test.bar"))
-      new Configuration("test") whichPath("test.test.bar") should equal (Some("test.test.bar"))
+      new Configuration("test") whichPath("foo") should equal (Some("test.foo"))
+      new Configuration("test") whichPath("foo.bar") should equal (Some("test.foo.bar"))
+      new Configuration("test") whichPath("bar") should equal (Some("test.bar"))
+      new Configuration("test") whichPath("test.foo.bar") should equal (Some("test.foo.bar"))
 
-      new Configuration("") whichPath("test", "test") should equal (Some("test.test"))
-      new Configuration("") whichPath("test.test", "test") should equal (Some("test.test"))
-      new Configuration("") whichPath("test.bar", "test") should equal (Some("test.test.bar"))
-      new Configuration("") whichPath("test.test.bar", "test") should equal (Some("test.test.bar"))
+      new Configuration("") whichPath("foo", "test") should equal (Some("test.foo"))
+      new Configuration("") whichPath("foo.bar", "test") should equal (Some("test.foo.bar"))
+      new Configuration("") whichPath("bar", "test") should equal (Some("test.bar"))
+      new Configuration("") whichPath("test.foo.bar", "test") should equal (Some("test.foo.bar"))
 
-      new Configuration("test") whichPath("bar", "test") should equal (Some("test.test.bar"))
-      new Configuration("test") whichPath("test.test", "bar") should equal (Some("test.test"))
-      new Configuration("test") whichPath("test.bar", "bar") should equal (Some("test.test.bar"))
-      new Configuration("test") whichPath("test.test.bar", "bar") should equal (Some("test.test.bar"))
+      new Configuration("test") whichPath("bar", "foo") should equal (Some("test.foo.bar"))
+      new Configuration("test") whichPath("foo", "bar") should equal (Some("test.foo"))
+      new Configuration("test") whichPath("bar", "spec") should equal (Some("spec.bar"))
+      new Configuration("test") whichPath("bar", "spec2") should equal (Some("test.bar"))
+      new Configuration("test") whichPath("test.foo.bar", "spec") should equal (Some("test.foo.bar"))
     }
 
     it("should be possible to retrieve default and specific values") {
