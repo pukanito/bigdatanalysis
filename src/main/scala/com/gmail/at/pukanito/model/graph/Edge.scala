9e01a93e7e97fa7ecd3e2a86bb992268a95aac6e
Pukanito Forager <pukanito@gmail.com>
2013-04-08 13:52:47 +0200

    Introduce Edge in Graph (again, but now really)

---
+++ src/main/scala/com/gmail/at/pukanito/model/graph/Edge.scala
@@ -0,0 +1,10 @@
+package com.gmail.at.pukanito.model.graph
+
+/**
+ * An edge is a relationship between two nodes.
+ *
+ * @param T the type that can be contained within the graph this edge belongs to.
+ */
+class Edge[T] {
+
+}
---
+++ src/test/scala/com/gmail/at/pukanito/model/graph/EdgeTest.scala
@@ -0,0 +1,16 @@
+package com.gmail.at.pukanito.model.graph
+
+import org.scalatest.FunSpec
+import org.scalatest.matchers.ShouldMatchers
+
+class EdgeTest extends FunSpec with ShouldMatchers {
+
+  describe("Edge") {
+
+    it("should connect parent node to child node") (pending)
+
+    it("should connect child node to parent node") (pending)
+
+  }
+
+}
